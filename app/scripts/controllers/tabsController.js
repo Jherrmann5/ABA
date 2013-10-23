e946f3f78dfbd07d3436259ed7109aa8a225e37c
Joel Herrmann <jherrmann5@gmail.com>
2013-10-23 11:30:39 -0500

    Temporary Drink Queue implemented, tabs page implemented

--- app/scripts/controllers/tabsController.js
+++ app/scripts/controllers/tabsController.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('ABAApp')
-  .controller('TabsController', function ($scope) {
-    //empty!
-  });
+.controller('TabsController', function ($scope) {
+	$scope.customers = [
+		{'name':'Matt Elliott', 'amount':'62.50'},
+		{'name':'Joel Herrmann', 'amount':'17.00'}
+	];
+});
