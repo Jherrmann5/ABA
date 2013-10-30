569254bf06500c0a5ee71cc05e0865f336a03634
Joel Herrmann <jherrmann5@gmail.com>
2013-10-30 15:39:05 -0500

    Changes a few merge conflicts.

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -1,30 +1,25 @@
 'use strict';
 
-angular.module('ABAApp')
-  .controller('HomeController', ['$scope', function ($scope) {
-    $scope.drinks = [
-    	'Rum & Coke',
-    	'Whiskey Sour',
-    	'Gin & Tonic',
-    	'Whiskey on the Rocks'
-    ];
-    $scope.queues =[
-    	'Pouring',
-    	'On Deck',
-    	'In the Hole',
-    	'If we are lucky',
-    	'#drunk',
-    	'blackout'
-    ];
-  }]);
-
 ABAApp.controller('HomeController', ['$scope', 'Drinks', function ($scope, Drinks) {
-    Drinks.updateDrinks();
-    $scope.drinks = Drinks.getDrinks();
-    alert("updated");
+	$scope.drinks = [
+		'Rum & Coke',
+		'Whiskey Sour',
+		'Gin & Tonic',
+		'Whiskey on the Rocks'
+	];
+	$scope.queues =[
+		'Pouring',
+		'On Deck',
+		'In the Hole',
+		'If we are lucky',
+		'#drunk',
+		'blackout'
+	];
+	Drinks.updateDrinks();
+	$scope.drinks = Drinks.getDrinks();
 
-    $scope.updateDrinks = function() {
-    	Drinks.updateDrinks();
-    	$scope.drinks = Drinks.getDrinks();
-    }
-  }]);
+	$scope.updateDrinks = function() {
+		Drinks.updateDrinks();
+		$scope.drinks = Drinks.getDrinks();
+	}
+}]);
